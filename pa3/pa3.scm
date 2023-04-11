97a09593f5d57c18f4e2bdb581a424bd848ee9fa
Sonaz M Jazny <sjazny@hydra1.eecs.utk.edu>
2023-04-11 09:15:05 -0400

    1, 2, 3

--- pa3/pa3.scm
+++ pa3/pa3.scm
@@ -9,15 +9,13 @@
 		(append (inc-list (- n 1)) (list n))))
 
 (define (rev-list l)
-	(if (null? l)
-		l
-		(begin
-		(cons (car l) l)
-		(rev-list (cdr l)))))
+	(if (= (length l) 0)
+		'()
+		(append (rev-list (cdr l)) (list (car l)))))
 
 (define (my-map f l)
-    #f ;; TODO: return something other than FALSE
-    )
+	
+	)
 
 (define (merge-sort l)
     ;; Split a list into two halves, returned in a pair. You may uncomment this.
