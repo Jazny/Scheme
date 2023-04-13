aefaea54e113f3d5a4a116c2f144abec328cae75
Sonaz M Jazny <sjazny@hydra1.eecs.utk.edu>
2023-04-13 01:06:12 -0400

    Scheme finished

--- pa3/pa3.scm
+++ pa3/pa3.scm
@@ -18,6 +18,10 @@
 		l
 		(cons (apply f (list (car l))) (my-map f (cdr l)))))
 
+
+
+
+
 (define (merge-sort l)
     ;; Split a list into two halves, returned in a pair. You may uncomment this.
     (define (split l)
@@ -28,6 +32,25 @@
                     (split-rec (cons (append front (list (car back))) (cdr back))))))
         (split-rec (cons (list (car l)) (cdr l))))
 
-	;; base case:
-	;; else, recurse:
-    )
+	;; Merge two pairs into one sorted list:
+	(define (merge left_list right_list)
+		;; base case: both lists are empty. return
+		(if (null? right_list)
+			left_list
+			(if (null? left_list)
+				right_list
+				(if (and (null? left_list) (null? right_list))
+					'()
+					;; if left greater than right, 
+					(if (<= (car right_list) (car left_list))
+						;; append to (car right_list), recurse left_list, cdr right list.
+						(append (list (car right_list)) (merge left_list (cdr right_list)))
+						;; else, recursively call merge on (cdr left_list, right_list)
+						(append (list (car left_list)) (merge (cdr left_list) right_list)))))))
+
+	(if (null? l)
+		'()
+		(if (null? (cdr l))
+			l
+			(let ((splat (split l)))
+				(merge (merge-sort (car splat)) (merge-sort (cdr splat)))))))
